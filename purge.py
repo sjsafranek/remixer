1bb51ed0d27bccc49162fd382f23b380a35fca7b
Stefan Safranek <stefan@stefan>
2020-10-25 17:01:58 -0700

    added purge.py to delete all songs from database

---
+++ purge.py
@@ -0,0 +1,25 @@
+import json
+from src import cmd
+from src import database
+
+
+if __name__ == "__main__":
+    parser = cmd.getArgumentParser()
+    args = parser.parse_args()
+
+    db = database.Database(
+        host=args.dbhost,
+        port=args.dbport,
+        dbname=args.dbname,
+        user=args.dbuser,
+        password=args.dbpass
+    )
+
+    songs = db.getSongs()
+    iterator = iter(songs)
+    while True:
+        try:
+            song = next(iterator)
+            song.delete()
+        except StopIteration:
+            break
